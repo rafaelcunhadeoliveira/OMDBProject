0af0c8b4c2141715b01d2c5395120ae6b6c2a4f3
Rafael Cunha de Oliveira <Rafael@Rafaels-MacBook-Pro.local>
2017-02-14 10:46:58 -0200

    Initial Commit

---
+++ OMDBProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OMDBProject
+//
+//  Created by Rafael Cunha de Oliveira on 14/02/17.
+//  Copyright © 2017 Rafael Cunha de Oliveira. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ OMDBProject/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  OMDBProject
+//
+//  Created by Rafael Cunha de Oliveira on 14/02/17.
+//  Copyright © 2017 Rafael Cunha de Oliveira. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
